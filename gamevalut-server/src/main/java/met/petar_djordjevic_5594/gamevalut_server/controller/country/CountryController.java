478c22b6ebd1419146d418ea2c7f6024b912e02d
petaaar88 <pe.bn80@gmail.com>
2025-01-27 15:00:54 +0100

    Remove country

--- gamevalut-server/src/main/java/met/petar_djordjevic_5594/gamevalut_server/controller/country/CountryController.java
+++
@@ -1,7 +0,0 @@
-package met.petar_djordjevic_5594.gamevalut_server.controller.country;
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class CountryController {
-}
--- gamevalut-server/src/main/java/met/petar_djordjevic_5594/gamevalut_server/repository/country/ICountryRepository.java
+++
@@ -1,9 +0,0 @@
-package met.petar_djordjevic_5594.gamevalut_server.repository.country;
-
-import met.petar_djordjevic_5594.gamevalut_server.model.country.Country;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface ICountryRepository extends JpaRepository<Country, Integer> {
-}
