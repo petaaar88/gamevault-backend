f1be768c070ed120ae7193f432bf2528bf7917ca
petaaar88 <pe.bn80@gmail.com>
2025-09-26 10:00:03 +0200

    Update cors allowed origins

--- gamevalut-server/src/main/java/met/petar_djordjevic_5594/gamevalut_server/security/SecurityConfig.java
+++ gamevalut-server/src/main/java/met/petar_djordjevic_5594/gamevalut_server/security/SecurityConfig.java
@@ -78,7 +78,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOriginPatterns(Arrays.asList("http://localhost:3000", "http://localhost:4200", "http://localhost:8080"));
+        configuration.setAllowedOriginPatterns(Arrays.asList("http://localhost:3000", "http://localhost:4200", "http://localhost:8080","http://localhost:5173"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
